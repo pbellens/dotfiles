c6e2a9b2a1dcb2378146f3604088a6a2acfad17b
pbellens <pbellens@gmail.com>
2022-08-22 10:23:47 +0200

    Update lspconfig.lua

--- nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -3,7 +3,6 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-<<<<<<< HEAD
 lspconfig["clangd"].setup {         
   on_attach = on_attach,         
   capabilities = capabilities,         
@@ -26,38 +25,3 @@ lspconfig["pyright"].setup {
     debounce_text_changes = 150,         
   },      
 }
-=======
-   -- lspservers with default config
-  local servers = { "clangd", "rust_analyzer" }
-  
-  lspconfig["clangd"].setup {         
-    on_attach = attach,         
-    capabilities = capabilities,         
-    flags = {            
-      debounce_text_changes = 150,         
-    },
-    cmd = {
-      "clangd",
-      "--background-index",
-      "--suggest-missing-includes",
-      "-j=4"
-    },
-    filetypes = {"c", "cpp", "cxx", "h", "hpp", "hxx"}
-  }
-
-  lspconfig["pyright"].setup {         
-      on_attach = attach,         
-      capabilities = capabilities,         
-      flags = {            
-        debounce_text_changes = 150,         
-      },      
-  }   
-  
-  -- typescript
-  -- lspconfig.tsserver.setup {      on_attach = function(client, bufnr)         client.resolved_capabilities.document_formatting = false         vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})      end,   }
-  -- the above tsserver config will remvoe the tsserver's inbuilt formatting -- since I use null-ls with denofmt for formatting ts/js stuff.end
-  
-end
-
-return M
->>>>>>> 4efafc60fd0e38092a6cee476196dc11eec7de5c
