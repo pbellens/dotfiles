f5654bd18486c4cb7aa100241fa8bad6c7dc062d
pbellens <pbellens@gmail.com>
2023-08-22 15:38:42 +0200

    Update init.lua

--- nvim/.config/nvim/lua/custom/init.lua
+++ nvim/.config/nvim/lua/custom/init.lua
@@ -5,3 +5,13 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+vim.wo.relativenumber = true
+
+-- Indenting
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.listchars:append{trail = "-", eol = "↵"}
+vim.opt.list = true
